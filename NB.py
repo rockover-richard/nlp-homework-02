b2ecc720e9d428bfc078f2ca93539000e4190525
Grouse <byung.channy.kim@gmail.com>
2019-10-15 20:08:01 -0400

    First commit

---
+++ NB.py
@@ -0,0 +1,20 @@
+'''
+NB.py should take the following parameters:
+the training file, the test files,
+the file where the parameters of the resulting model will be saved,
+and the output file where you will write predictions
+made by the classifier on the test data (one example per line).
+
+The last line in the output file should list the overall accuracy
+of the classifier on the test data.
+
+The training and test files should have the following format:
+one example per line; each line corresponds to an example;
+first column is the label, and the other columns are feature values
+
+Train NB classifier on the training partition using BOW features
+(use add-one smoothing). Evaluate classifier on the test partition.
+
+Save parameters of your BOW model in a file called movie-review-BOW.NB.
+Report the accuracy of your program on the test data with BOW features.
+'''
